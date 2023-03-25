a0c7eab54514d686204cfe921238f585eab5d47c
KarlTamberg <tamberg.karl@gmail.com>
2023-03-25 18:08:23 +0200

    Move to class structure.

--- README.md
+++ README.md
@@ -7,8 +7,15 @@ Libraries:
 * Pillow
 
 References:
+* https://docs.python.org/3/library/tkinter.html
+* https://pillow.readthedocs.io/en/stable/reference/Image.html
+* https://pillow.readthedocs.io/en/stable/reference/ImageTk.html
+* https://pillow.readthedocs.io/en/stable/reference/ImageGrab.html
 * https://realpython.com/python-gui-tkinter/#building-your-first-python-gui-application-with-tkinter
 * https://www.tutorialspoint.com/python/tk_canvas.htm#
+* https://www.youtube.com/watch?v=Jl1xsH6MR1g&t=254s&ab_channel=Codemy.com
+* https://stackoverflow.com/questions/15839491/how-to-clear-tkinter-canvas
+* https://stackoverflow.com/questions/10865116/tkinter-creating-buttons-in-for-loop-passing-command-arguments
 
 
 python -m pip install pysimplegui
--- bpmn_editor.py
+++ bpmn_editor.py
@@ -1,104 +1,115 @@
 import tkinter as tk
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 from PIL import ImageTk, Image, ImageGrab
+import os
 
 
-def open_file():
-    """Open a file for editing."""
-    global img  # Making img a global variable, might not be needed
-    global photo  # Making photo a global variable, otherwise gets garbage-collected
-    filepath = askopenfilename(
-        filetypes=[("PNG Files", "*.png"), ("All Files", "*.*")]
-    )
-    if not filepath:
-        return
-    img = Image.open(filepath)
-    photo = ImageTk.PhotoImage(img)
-    # Configuring the canvas to have a scroll region that matches the image size
-    canvas.config(scrollregion=(0, 0, img.width, img.height))
-    canvas.create_image(0, 0, image=photo, anchor="nw")
+class BpmnEditor:
+    def __init__(self, window):
+        window.title("BPMN Editor")
+
+        self.diagram_img = None  # The base BPMN diagram
+        self.diagram = None  # The base BPMN diagram
+        self.icon_list = []  # Icon image files added to the diagram
+        self.drag_item = 0  # The icon object currently being dragged
+
+        window.rowconfigure(0, minsize=800, weight=1)
+        window.columnconfigure(1, minsize=800, weight=1)
+
+        # Creating a canvas with scrollbar widget
+        vscrollbar = tk.Scrollbar(window)
+        vscrollbar.grid(row=0, column=2, sticky=tk.NS)
+        # Creating a horizontal scrollbar widget
+        hscrollbar = tk.Scrollbar(window, orient="horizontal")
+        hscrollbar.grid(row=1, column=1, sticky=tk.EW)
+        # Creating a canvas with the scrollbars attached
+        self.canvas = tk.Canvas(window, width=800, height=600, yscrollcommand=vscrollbar.set,
+                                xscrollcommand=hscrollbar.set)
+        self.canvas.grid(row=0, column=1, sticky=tk.NSEW)
+        # Configuring the scrollbars to scroll the canvas
+        vscrollbar.config(command=self.canvas.yview)
+        hscrollbar.config(command=self.canvas.xview)
+
+        # Add buttons to the left side
+        frm_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
+        btn_open = tk.Button(frm_buttons, text="Open", command=self.open_file)
+        btn_open.grid(row=0, column=0, sticky=tk.EW, padx=5, pady=5)
+        btn_save = tk.Button(frm_buttons, text="Save As...", command=self.save_file)
+        btn_save.grid(row=1, column=0, sticky=tk.EW, padx=5, pady=5)
+        self.button_icons = []
+        self.buttons = []
+        row = 2
+        for filename in os.listdir("assets"):
+            original_img = Image.open("assets/" + filename)
+            resized_img = original_img.resize((35, 35))  # resize image
+            img = ImageTk.PhotoImage(resized_img)
+            self.button_icons.append(img)
+            btn_add_lock = tk.Button(
+                frm_buttons, text="Add " + filename, image=img, command=lambda fn=filename: self.add_icon(fn)
+            )
+            btn_add_lock.grid(row=row, column=0, sticky=tk.EW, padx=5, pady=5)
+            self.buttons.append(btn_add_lock)
+            row += 1
+        frm_buttons.grid(row=0, column=0, sticky=tk.NS)
+
+        # bind mouse events for dragging and dropping
+        self.canvas.bind("<Button-1>", self.drag_start)
+        self.canvas.bind("<B1-Motion>", self.drag_move)
+
+    def create_buttons(self):
+        pass
+
+    def open_file(self):
+        """Open a file for editing."""
+        filepath = askopenfilename(filetypes=[("PNG Files", "*.png"), ("All Files", "*.*")])
+        if not filepath:
+            return
+        self.canvas.delete("all")  # reset the canvas
+        self.diagram_img = Image.open(filepath)
+        self.diagram = ImageTk.PhotoImage(self.diagram_img)
+        # Todo: resize canvas + maybe resize window?
+        # Configuring the canvas to have a scroll region that matches the image size
+        self.canvas.config(scrollregion=(0, 0, self.diagram_img.width, self.diagram_img.height))
+        self.canvas.create_image(0, 0, image=self.diagram, anchor="nw")
+
+    def save_file(self):
+        """Save the file."""
+        filepath = asksaveasfilename(defaultextension='.png')
+        if not filepath:
+            return
+        ImageGrab.grab(bbox=(
+            self.canvas.winfo_rootx(),
+            self.canvas.winfo_rooty(),
+            self.canvas.winfo_rootx() + self.canvas.winfo_width(),
+            self.canvas.winfo_rooty() + self.canvas.winfo_height()
+        )).save(filepath)
+
+    def add_icon(self, icon_name):
+        """Add an icon on a canvas."""
+        if not self.diagram:  # block while no base image opened
+            print("Open base image before adding icon")
+            return
+        img = Image.open("assets/" + icon_name)
+        img.thumbnail((40, 40))
+        photo = ImageTk.PhotoImage(img)
+        self.icon_list.append(photo)
+        self.canvas.create_image(50, 50, image=photo)
+
+    def drag_start(self, event):
+        """Find the object to drag."""
+        closest_items = self.canvas.find_closest(event.x, event.y)
+        if len(closest_items) == 0:
+            return
+        self.drag_item = closest_items[0]
+
+    def drag_move(self, event):
+        """Drag an object."""
+        items = self.canvas.find_withtag("all")  # get all item ids
+        if len(items) > 1:  # Block moving the base image
+            self.canvas.coords(self.drag_item, event.x, event.y)
 
 
-def save_file():
-    """Save the file."""
-    filepath = asksaveasfilename(defaultextension='.png')
-    if not filepath:
-        return
-    ImageGrab.grab(bbox=(
-        canvas.winfo_rootx(),
-        canvas.winfo_rooty(),
-        canvas.winfo_rootx() + canvas.winfo_width(),
-        canvas.winfo_rooty() + canvas.winfo_height()
-    )).save(filepath)
-    #img.save(filepath)
-
-
-def add_lock():
-    # load another image for testing
-    img = Image.open("images/lock.png")
-    img.thumbnail((50, 50))
-    photo = ImageTk.PhotoImage(img)
-    added_images.append(img)
-    added_photos.append(photo)
-    canvas.create_image(50, 50, image=photo)
-
-
-def add_unlock():
-    # load another image for testing
-    img = Image.open("images/unlock.png")
-    img.thumbnail((50, 50))
-    photo = ImageTk.PhotoImage(img)
-    added_images.append(img)
-    added_photos.append(photo)
-    canvas.create_image(50, 50, image=photo)
-
-
-def drag_start(event):
-    global drag_item
-    drag_item = canvas.find_closest(event.x, event.y)[0]
-
-
-def drag_move(event):
-    global drag_item
-    canvas.coords(drag_item, event.x, event.y)
-
-
-added_images = []
-added_photos = []
-window = tk.Tk()
-window.title("BPMN Editor")
-
-window.rowconfigure(0, minsize=800, weight=1)
-window.columnconfigure(1, minsize=800, weight=1)
-
-# Creating a vertical scrollbar widget
-vscrollbar = tk.Scrollbar(window)
-vscrollbar.grid(row=0, column=2, sticky="ns")
-
-# Creating a horizontal scrollbar widget
-hscrollbar = tk.Scrollbar(window, orient="horizontal")
-hscrollbar.grid(row=1, column=1, sticky="ew")
-
-# Creating a canvas with the scrollbars attached
-canvas = tk.Canvas(window, width=800, height=600, yscrollcommand=vscrollbar.set, xscrollcommand=hscrollbar.set)
-canvas.grid(row=0, column=1, sticky="nsew")
-
-# Configuring the scrollbars to scroll the canvas
-vscrollbar.config(command=canvas.yview)
-hscrollbar.config(command=canvas.xview)
-
-frm_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
-btn_open = tk.Button(frm_buttons, text="Open", command=open_file)
-btn_save = tk.Button(frm_buttons, text="Save As...", command=save_file)
-btn_add_lock = tk.Button(frm_buttons, text="Add lock", command=add_lock)
-btn_add_unlock = tk.Button(frm_buttons, text="Add unlock", command=add_unlock)
-btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
-btn_save.grid(row=1, column=0, sticky="ew", padx=5, pady=5)
-btn_add_lock.grid(row=2, column=0, sticky="ew", padx=5, pady=5)
-btn_add_unlock.grid(row=3, column=0, sticky="ew", padx=5, pady=5)
-frm_buttons.grid(row=0, column=0, sticky="ns")
-# bind mouse events for dragging and dropping
-canvas.bind("<Button-1>", drag_start)
-canvas.bind("<B1-Motion>", drag_move)
-
-window.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    BpmnEditor(root)
+    root.mainloop()
